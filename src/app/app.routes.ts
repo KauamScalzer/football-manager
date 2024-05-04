a5ee3ac26cbce3e85ceda6f4c794619ad13469eb
Kauam Scalzer <kauam.souza@matrixdobrasil.com.br>
2024-05-04 12:34:50 -0300

    feat: add team component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TeamsComponent } from './teams/teams.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent}
+  { path: '', component: HomeComponent},
+  { path: 'teams', component: TeamsComponent}
 ];
