2198c8dcffc6a588cc14d5bd51f126992c2d5353
Kauam Scalzer <kauam.souza@matrixdobrasil.com.br>
2024-05-16 19:21:14 -0300

    feat: add login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { TeamsComponent } from './teams/teams.component';
 import { TableComponent } from './table/table.component';
 import { MatchesComponent } from './matches/matches.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'teams', component: TeamsComponent},
   { path: 'table', component: TableComponent},
-  { path: 'matches', component: MatchesComponent}
+  { path: 'matches', component: MatchesComponent},
+  { path: 'login', component: LoginComponent }
 ];
