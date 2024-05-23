3740503dc50f9ba81a715f523f1921ba9d32b36a
Kauam Scalzer <kauam.souza@matrixdobrasil.com.br>
2024-05-23 20:58:59 -0300

    feat: add show team

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,20 +3,42 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { ShowTeamComponent } from '../showTeam/showTeam.component';
 import axios from 'axios';
+import { CommonModule } from '@angular/common';
+
+interface Teams {
+  id: number;
+  name: string;
+  urlImage: string;
+}
+
+interface TeamResult {
+  count: number;
+  result: Teams[]
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, ShowTeamComponent],
+  imports: [NavbarComponent, FooterComponent, ShowTeamComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  teamImageUrl: string = "https://s.sde.globo.com/media/organizations/2018/04/10/Flamengo-2018.svg";
-  teamName: string = "Flamengo";
+  teams: Teams[]
+
+  ngOnInit(): void {
+    this.getTeams()
+  }
 
   async getTeams() {
-    const teams = await axios.get("localhost:3000/team?take=10&skip=1")
-    console.log(teams)
+    const apiResult = await axios.get('http://localhost:3000/team', {
+      params: {
+        skip: 1,
+        take: 10
+      }
+    })
+    const teams: TeamResult = apiResult.data
+    console.log(teams.result)
+    this.teams = teams.result
   }
 }
