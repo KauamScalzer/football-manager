cd6659570ad6363c7fdc544e64666af19cb6d706
Kauam Scalzer <kauam.souza@matrixdobrasil.com.br>
2024-05-23 17:03:45 -0300

    feat: add login route methods

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  constructor(
+    private router: Router
+  ) {}
+
+  camposPreenchidos: boolean = false;
+  usuario: string = '';
+  senha: string = '';
+  
+  verificarPreenchimento() {
+    this.camposPreenchidos = this.usuario.trim() !== '' && this.senha.trim() !== '';
+  }
+
+  async login() {
+    const result = await axios.post('http://localhost:3000/user/login', {
+      username: this.usuario,
+      password: this.senha
+    })
+    if (result.status === 200) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  redirectToSignup() {
+    this.router.navigate(['/signup']);
+  }
 }
