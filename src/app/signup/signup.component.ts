d85785fa9371606b897ab1ec8f395f10a5735b04
Kauam Scalzer <kauam.souza@matrixdobrasil.com.br>
2024-05-23 17:26:13 -0300

    feat: add signup page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  constructor(
+    private router: Router
+  ) {}
 
+  camposPreenchidos: boolean = false;
+  usuario: string = '';
+  senha: string = '';
+  passwordConfirmation: string = '';
+
+  verificarPreenchimento() {
+    this.camposPreenchidos = this.usuario.trim() !== '' && this.senha.trim() !== '' && this.passwordConfirmation.trim() !== '';
+  }
+
+  async signup() {
+    const result = await axios.post('http://localhost:3000/user/signup', {
+      username: this.usuario,
+      password: this.senha
+    })
+    if (result.status === 200) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  redirectToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
