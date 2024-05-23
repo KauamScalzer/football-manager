7f717534b20e5d5b33c31535a23f0bb33a15aeaa
Kauam Scalzer <kauam.souza@matrixdobrasil.com.br>
2024-05-23 19:37:01 -0300

    feat: add show team component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { ShowTeamComponent } from '../showTeam/showTeam.component';
+import axios from 'axios';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, ShowTeamComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  teamImageUrl: string = "https://s.sde.globo.com/media/organizations/2018/04/10/Flamengo-2018.svg";
+  teamName: string = "Flamengo";
 
+  async getTeams() {
+    const teams = await axios.get("localhost:3000/team?take=10&skip=1")
+    console.log(teams)
+  }
 }
---
+++ src/app/showTeam/showTeam.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-showTeam',
+  standalone: true,
+  imports: [NavbarComponent, FooterComponent],
+  templateUrl: './showTeam.component.html',
+  styleUrl: './showTeam.component.css'
+})
+export class ShowTeamComponent {
+  @Input() teamImageUrl: string;
+  @Input() teamName: string;
+}
